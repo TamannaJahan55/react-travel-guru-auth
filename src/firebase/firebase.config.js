333cef359011bc59d6ea3287b6c17b22845b7b21
TamannaJahan55 <tamannajahan.mis049@gmail.com>
2023-10-06 12:25:37 +0600

    project set up

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBVDmX1gowoMsDKoniYrOM_umvd49IDlyU",
+  authDomain: "react-travel-guru-auth-5fa33.firebaseapp.com",
+  projectId: "react-travel-guru-auth-5fa33",
+  storageBucket: "react-travel-guru-auth-5fa33.appspot.com",
+  messagingSenderId: "653429348242",
+  appId: "1:653429348242:web:9c4050c914b95ed36f934b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
